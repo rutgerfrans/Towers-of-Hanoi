3a37a21cfcd05e0cfe8610d695785bb40fa2be46
Rutger de Groen <rutger.degroen@ivengi.com>
2021-12-06 02:32:49 +0100

    code added

---
+++ PI7TowerOfHanoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PI7TowerOfHanoi
+{
+    class Program
+    {
+        public static void Main()
+        { 
+            int Schijven = 4;
+
+            Hanoi(Schijven, 'S', 'A', 'D');
+        }
+
+        static void Hanoi(int Schijven, char Bron, char Bestemming, char Hulp)
+        {
+            if (Schijven == 1)
+            {
+                Console.WriteLine("Zet Schijf 1 van Stapel " + Bron + " Naar Stapel " + Bestemming);
+                return;
+            }
+            Hanoi(Schijven - 1, Bron, Hulp, Bestemming);
+            Console.WriteLine("Zet Schijf " + Schijven + " Van Stapel " + Bron + " Naar Stapel " + Bestemming);
+            Hanoi(Schijven - 1, Hulp, Bestemming, Bron);
+        }
+    }
+}
